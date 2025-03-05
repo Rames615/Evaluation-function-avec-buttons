be26e9b8bdbf7ef132be7a828f14dc9a3ffad04d
Rames615 <ramostrick@gmx.com>
2025-03-05 17:10:21 +0100

    ajouter les bouttons suprimer et reset

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,48 @@
+console.log('hello world');
+const button = document.getElementById('btn');
 const button1 = document.getElementById('btn1');
 const button2 = document.getElementById('btn2');
-const form = document.querySelector('.formulaire');
+const form = document.querySelector('#formulaire');
+//const form = document.querySelector('form');
+
+// way to reset the filled form
+
+button.addEventListener('click', function() {
+    form.reset();
+  });
+  
 
 function addCard(e){
     e.preventDefault();
 const card = document.createElement('div');
 card.className = 'card';
-const titre = document.createElement('title');
-titre.textContent = "Mon titre";
+
+const image = document.createElement('img');
+// const image = document.querySelector('#image');
+ image.src =image.value //"https://picsum.photos/1200?random=4";
+
 const paragraph = document.createElement('p');
-paragraph.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+ paragraph.textContent = //"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
 
 card.appendChild(titre);
+card.appendChild(image);
+
 card.appendChild(paragraph);
 form.appendChild(card);
 }
 
 button1.addEventListener('click',addCard);
+
+function removeCard(e) {
+    e.preventDefault();
+    const deleteCard = form.lastChild;
+    if(deleteCard){
+        deleteCard.remove();
+    } else {
+        alert("Aucune carte à supprimer !");
+    }
+    
+}
+
+button2.addEventListener('click',removeCard);
+
